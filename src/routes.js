df6648f53fda24651fc1e88256e352853fb9adf1
Sanoop-s <sanoopsid5@gmail.com>
2024-11-11 18:19:50 +0530

    path change

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import ToDoList from "./components/ToDoList.vue";
 
 const routes = [
-    { path: "/todo-with-props-emit/", redirect:'/to-do/Add task' },
-    { path: "/to-do/:heading", component: ToDoList}
+    { path: "/", redirect:'/to-do/Addtask' },
+    { path: "/to-do/:heading", component:() => ToDoList}
 ]
  
 export const router = createRouter({
