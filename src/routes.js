25c7af866e94e9ca9ec85009853a2a2cba0828f1
Sanoop-s <sanoopsid5@gmail.com>
2024-11-12 10:25:12 +0530

    routes added

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,10 @@ import ToDoList from "./components/ToDoList.vue";
 import Home from "./components/Home.vue";
 
 const routes = [
-    { path: "/",component:Home, name:"home" },
-    { path: "/to-do", component:() => ToDoList,name:'todo'}
+    { path: "/", component: Home, name: "home" },
+    { path: "/to-do", component: ToDoList, name: 'todo' }
 ]
- 
+
 export const router = createRouter({
     history: createWebHistory('todo-with-props-emit'),
     routes
